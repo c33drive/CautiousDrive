b9aa5ea279b4cbb1194f201b68ceca0938c12a96
phishekwa <phishekwa@gmail.com>
2018-03-04 15:38:35 +0200

    The first upload

---
+++ app/src/main/java/cautiousdrive/com/cautiousdrive/MainActivity.java
@@ -0,0 +1,134 @@
+package cautiousdrive.com.cautiousdrive;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Dialog.*;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ImageButton btnAccident, btnTrafficCong, btnTrafficLights, btnPronedZone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.cd_logo);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        setContentView(R.layout.activity_main);
+
+        btnPronedZone = (ImageButton) findViewById(R.id.btn_proneZone);
+        btnPronedZone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder theDialog = new AlertDialog.Builder(MainActivity.this);
+                theDialog.setMessage("Are you sure you want to send an alert on HIGH ACCIDENT ZONE?");
+                theDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Sharing location methond
+                        Toast.makeText(MainActivity.this, "YOUR ALERT WAS SENT",Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                theDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                            return;
+                    }
+                });
+
+                AlertDialog myDialog = theDialog.create();
+                myDialog.show();
+            }
+        });
+
+        btnAccident = (ImageButton) findViewById(R.id.accident);
+        btnAccident.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder theDialog = new AlertDialog.Builder(MainActivity.this);
+                theDialog.setMessage("Are you sure you want to send an alert on ACCIDENT?");
+                theDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Sharing location methond
+                        Toast.makeText(MainActivity.this, "YOUR ALERT WAS SENT",Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                theDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        return;
+                    }
+                });
+
+                AlertDialog myDialog = theDialog.create();
+                myDialog.show();
+            }
+        });
+
+        btnTrafficLights = (ImageButton) findViewById(R.id.traffic_lights);
+        btnTrafficLights.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder theDialog = new AlertDialog.Builder(MainActivity.this);
+                theDialog.setMessage("Are you sure you want to send an alert on TRAFFIC LIGHTS");
+                theDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Sharing location methond
+                        Toast.makeText(MainActivity.this, "YOUR ALERT WAS SENT",Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                theDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        return;
+                    }
+                });
+
+                AlertDialog myDialog = theDialog.create();
+                myDialog.show();
+            }
+        });
+
+        btnTrafficCong = (ImageButton) findViewById(R.id.traffic_cong);
+        btnTrafficCong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder theDialog = new AlertDialog.Builder(MainActivity.this);
+                theDialog.setMessage("Are you sure you want to send an alert on TRAFFIC CONGESTION?");
+                theDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Sharing location methond
+                        Toast.makeText(MainActivity.this, "YOUR ALERT WAS SENT",Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                theDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        return;
+                    }
+                });
+
+                AlertDialog myDialog = theDialog.create();
+                myDialog.show();
+            }
+        });
+
+    }
+}
